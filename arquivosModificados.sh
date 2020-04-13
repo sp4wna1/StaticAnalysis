54bb40b8a3fb0936e186b8a1634d72ce9a7829de
Gabriel Danilo do Prado <gaa.prado@hotmail.com>
2020-04-12 22:16:44 -0300

    Updating

--- arquivosModificados.sh
+++ arquivosModificados.sh
@@ -1,10 +1,15 @@
 declare -a myArray=$(echo $(git diff $1 --diff-filter=d --name-only --relative | grep '\.kt[s"]\?$'))
 repo_root=$(echo $(pwd))
 last_occurrence=$(echo ${myArray} | sed 's#.* ##g')
+if [[ -z "$myArray" ]]; then
+mkdir -p ${repo_root}/app/build/reports/$2/
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?><checkstyle version=\"8.0\"/>" >> ${repo_root}/app/build/reports/$2/$2-report.xml
+else
 for file in ${myArray}; do
-if [[ "$file" == "$last_occurrence" || $2 == 'ktlint' ]]; then
+if [[ "$file" == "$last_occurrence" || $1 == 'ktlint' ]]; then
 echo ${repo_root}/${file};
 else
 echo ${repo_root}/${file},;
 fi
 done
+fi
